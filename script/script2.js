7d70ef1e6c4569c03d5b0f1a3079b776c336c861
lYugol <156240240+lYugol@users.noreply.github.com>
2024-05-21 11:58:27 +0300

    last

--- script/script2.js
+++ script/script2.js
@@ -2,12 +2,14 @@ const popup = document.querySelector('.modal-call');
 const openPopup = document.querySelectorAll('.button-show');
 const closePopup = document.querySelector('.modal__call-close');
 
-openPopup.forEach((button)=>{
-    button.addEventListener('click', function(evt){
+for(let i = 0; i < openPopup.length ; i++){
+openPopup[i].addEventListener('click', function(evt){
         evt.preventDefault();
         popup.classList.add('modal-call--open')
     });
+
+}
+
     closePopup.addEventListener('click', function(){
         popup.classList.remove('modal-call--open')
     })
-})
--- script/script3.js
+++ script/script3.js
@@ -1,12 +1,14 @@
 const popupFeedback = document.querySelector('.modal-feedback');
-const openPopupFeedback = document.querySelector('.button-ivent');
+const openPopupFeedback = document.querySelectorAll('.button-ivent');
 const closePopupFeedback = document.querySelector('.modal__feedback-close');
 
 
-openPopupFeedback.addEventListener('click', function(evt){
+for (let i = 0; i < openPopupFeedback.length; i++) {
+    openPopupFeedback[i].addEventListener('click', function(evt){
         evt.preventDefault();
         popupFeedback.classList.add('modal-feedback--open')
     });
+}
     closePopupFeedback.addEventListener('click', function(){
         popupFeedback.classList.remove('modal-feedback--open')
     })
