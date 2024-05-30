804eda20c8f862bbd36d39c4b8b71352d040c21e
lYugol <156240240+lYugol@users.noreply.github.com>
2024-05-30 16:50:07 +0300

    last

---
+++ webpack-static-template-master/src/js/burgerMenu.js
@@ -0,0 +1,21 @@
+const burgerMenu = document.querySelector('.burger-menu')
+const openBurgerMenu = document.querySelector('.header__burger-menu')
+const closeBurgerMenu = document.querySelector('.header__left--close')
+const burgerMenuBg = document.querySelector('.burger-menu-bg')
+
+openBurgerMenu.addEventListener('click', function (evt) {
+  evt.preventDefault()
+  burgerMenu.classList.add('burger-menu--open')
+  burgerMenuBg.classList.add('burger-menu-bg--open')
+})
+
+closeBurgerMenu.addEventListener('click', function () {
+  burgerMenu.classList.remove('burger-menu--open')
+  burgerMenuBg.classList.remove('burger-menu-bg--open')
+})
+
+burgerMenuBg.addEventListener('click', function (evt) {
+  if (evt.currentTarget === evt.target) {
+    burgerMenuBg.classList.remove('burger-menu-bg--open')
+  }
+})
